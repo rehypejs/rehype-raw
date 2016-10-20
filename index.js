dd9e410c39531e34690c26627a33360b5ac1bdf5
Titus Wormer <tituswormer@gmail.com>
2016-10-20 11:58:18 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* Dependencies. */
+var util = require('hast-util-raw');
+
+/* Expose. */
+module.exports = raw;
+
+function raw() {
+  return util;
+}
---
+++ test.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/* Dependencies. */
+var test = require('tape');
+var unified = require('unified');
+var parse = require('remark-parse');
+var remark2rehype = require('remark-rehype');
+var stringify = require('rehype-stringify');
+var raw = require('./');
+
+/* Tests. */
+test('integration', function (t) {
+  unified()
+    .use(parse)
+    .use(remark2rehype, {allowDangerousHTML: true})
+    .use(raw)
+    .use(stringify)
+    .process([
+      '<div class="note">',
+      '',
+      'A mix of *markdown* and <em>HTML</em>.',
+      '',
+      '</div>'
+    ].join('\n'), function (err, file) {
+      t.ifErr(err, 'should not fail');
+
+      t.equal(
+        String(file),
+        [
+          '<div class="note">',
+          '<p>A mix of <em>markdown</em> and <em>HTML</em>.</p>',
+          '</div>'
+        ].join('\n'),
+        'should equal the fixture'
+      );
+    });
+
+  t.end();
+});
