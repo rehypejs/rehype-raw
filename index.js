1af71171edd5a9216c9f71b1de17f7e5f8ae2ea7
Titus Wormer <tituswormer@gmail.com>
2021-08-02 09:31:07 +0200

    Add JSDoc based types

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+/**
+ * @typedef {import('hast').Root} Root
+ * @typedef {import('hast-util-raw').Options} Options
+ */
+
 import {raw} from 'hast-util-raw'
 
-export default function rehypeRaw(options) {
-  return transform
-  function transform(tree, file) {
-    return raw(tree, file, options)
+/**
+ * Plugin to parse the tree again (and raw nodes).
+ * Keeping positional info OK.  🙌
+ *
+ * @type {import('unified').Plugin<[Options?] | void[], Root>}
+ */
+export default function rehypeRaw(options = {}) {
+  return (tree, file) => {
+    // Assume that when a root was given, it’s also returned.
+    const result = /** @type {Root} */ (raw(tree, file, options))
+    return result
   }
 }
--- types/rehype-raw-tests.ts
+++
@@ -1,5 +0,0 @@
-import unified = require('unified')
-import raw = require('rehype-raw')
-
-unified().use(raw)
-unified().use(raw, {passThrough: []})
