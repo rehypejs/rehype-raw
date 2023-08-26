b1e7f8ee793471901230632de46635d5467993b1
Titus Wormer <tituswormer@gmail.com>
2023-08-26 11:54:05 +0200

    Refactor code-style

--- test.js
+++ test.js
@@ -2,42 +2,50 @@
  * @typedef {import('hast').Root} Root
  */
 
-import test from 'node:test'
 import assert from 'node:assert/strict'
-import {unified} from 'unified'
-import {visit} from 'unist-util-visit'
+import test from 'node:test'
+import rehypeStringify from 'rehype-stringify'
 import remarkParse from 'remark-parse'
 import remarkRehype from 'remark-rehype'
-import rehypeStringify from 'rehype-stringify'
+import {unified} from 'unified'
+import {visit} from 'unist-util-visit'
 import rehypeRaw from './index.js'
 
-const markdown = `<div class="note">
+test('rehypeRaw', async function (t) {
+  await t.test('should expose the public api', async function () {
+    assert.deepEqual(Object.keys(await import('./index.js')).sort(), [
+      'default'
+    ])
+  })
+
+  await t.test('should work', async function () {
+    const file = await unified()
+      // @ts-expect-error: to do: remove when remark is released.
+      .use(remarkParse)
+      // @ts-expect-error: to do: remove when remark-rehype is released.
+      .use(remarkRehype, {allowDangerousHtml: true})
+      .use(rehypeRaw)
+      .use(function () {
+        /**
+         * @param {Root} root
+         */
+        return function (root) {
+          visit(root, 'raw', function () {
+            assert.fail()
+          })
+        }
+      })
+      .use(rehypeStringify).process(`<div class="note">
 
 A mix of *markdown* and <em>HTML</em>.
 
-</div>`
+</div>`)
 
-const html = `<div class="note">
+    assert.equal(
+      String(file),
+      `<div class="note">
 <p>A mix of <em>markdown</em> and <em>HTML</em>.</p>
 </div>`
-
-test('rehypeRaw', async () => {
-  const file = await unified()
-    // @ts-expect-error: to do: remove when remark is released.
-    .use(remarkParse)
-    // @ts-expect-error: to do: remove when remark-rehype is released.
-    .use(remarkRehype, {allowDangerousHtml: true})
-    .use(rehypeRaw)
-    .use(
-      /** @type {import('unified').Plugin<Array<void>, Root>} */
-      () => (root) => {
-        visit(root, 'raw', () => {
-          assert.fail('should not include `raw` in tree after `rehype-raw`')
-        })
-      }
     )
-    .use(rehypeStringify)
-    .process(markdown)
-
-  assert.equal(String(file), html, 'should equal the fixture')
+  })
 })
